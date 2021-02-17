b23cd0a2431d885fc8632ae76a5ae077d8a8f007
Riley Howsden <42232154+rbhowsden@users.noreply.github.com>
2021-02-16 17:22:17 -0800

    Simplify

--- Rotating-Store/long_simulation.py
+++ Rotating-Store/long_simulation.py
@@ -6,7 +6,7 @@ plateau_dist = [.30, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 #EVERY PLAYER WILL HAVE DIFFERENT DISTRIBUTION
 
-r_cadence = 52
+r_num = 52
 r_size = 6
 c_size = 1000
 c_growth = 2
@@ -14,89 +14,38 @@ power_dist = [.20, .133, .0889, .059260, .03951, .02634, .017559, .011706, .0078
 
 import random
 
-def random_selection(dist, r_num, r_size, c_size, c_growth):
-
-    dist_length = len(dist)
-    if c_size % dist_length != 0:
-        print('bad, choose better parameters')
-
-    full_catalog = dist*int(c_size/dist_length)
-    items = [(i, x, x*random.random()*2) for i, x in enumerate(full_catalog)]
-    current_i = len(items)
-    random.shuffle(items)
-    
-    total_sold = 0
-    for _ in range(r_num):
-        for item in items[0:r_size]:
-            if random.random() <= item[2]:
-                items.remove(item)
-                total_sold += 1
-        new_items = random.sample(dist, c_growth)
-        new_items = [(current_i + i, x, x*random.random()*2) for i, x in enumerate(new_items)]
-        items += new_items
-        random.shuffle(items)
-        current_i += c_growth
-    
-    return total_sold
-
-
 def rotate_simulation(dist, r_num, r_size, c_size, c_growth, method):
 
-    dist_length = len(dist)
-    if c_size % dist_length != 0:
-        print('bad, choose better parameters')
-
-    full_catalog = dist*int(c_size/dist_length)
-    items = [(i, x, x*random.random()*2) for i, x in enumerate(full_catalog)]
-    current_i = len(items)
-    rank_items(items, method)
+    catalog = add_items([], c_size, dist, c_size)
+    catalog = rank_items(catalog, method)
+    index = c_size
 
     total_sold = 0
     for _ in range(r_num):
-        for item in items[0:r_size]:
+        for item in catalog[0:r_size]:
             if random.random() <= item[2]:
-                items.remove(item)
+                catalog.remove(item)
                 total_sold += 1
-        new_items = random.sample(dist, c_growth)
-        new_items = [(current_i + i, x, x*random.random()*2) for i, x in enumerate(new_items)]
-        items += new_items
-        rank_items(items, method)
-        current_i += c_growth
+        catalog = add_items(catalog, index, dist, c_growth)
+        index += c_growth
+        catalog = rank_items(catalog, method)
     
     return total_sold
 
-def rank_items(items, method):
+def rank_items(catalog, method):
     if method == "random":
-        random.shuffle(items)
+        random.shuffle(catalog)
     elif method == "popular":
-        items.sort(key=lambda x: x[1], reverse=True)
+        catalog.sort(key=lambda x: x[1], reverse=True)
     elif method == "personal":
-        items.sort(key=lambda x: x[2], reverse=True)
+        catalog.sort(key=lambda x: x[2], reverse=True)
 
-    return items
+    return catalog
 
-def personal_selection(dist, r_num, r_size, c_size, c_growth):
+def add_items(catalog, index, dist, number):
+    q, r = divmod(number, len(dist))
+    new_items = q*dist + random.sample(dist, r)
+    catalog = [(index + i, x, x*random.random()*2) for i, x in enumerate(new_items)]
 
-    total_sold = 0
-    dist_length = len(dist)
-    if c_size % dist_length != 0:
-        print('bad, choose better parameters')
+    return catalog
 
-    full_catalog = dist*int(c_size/dist_length)
-    items = [(i, x*random.random()*2) for i, x in enumerate(full_catalog)]
-    current_i = len(items)
-    items.sort(key=lambda x: x[1], reverse=True)
-
-    total_sold = 0
-    for _ in range(r_num):
-        for item in items[0:r_size]:
-            if random.random() <= item[1]:
-                items.remove(item)
-                total_sold += 1
-        new_items = random.sample(dist, c_growth)
-        new_items = [(current_i + i, x*random.random()*2) for i, x in enumerate(new_items)]
-        items += new_items
-        items.sort(key=lambda x: x[1], reverse=True)
-        current_i += c_growth
-    
-    return total_sold
